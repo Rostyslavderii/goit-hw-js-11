3f5105bc6d1e22b68bd9c0b659f7c1062aa41ab1
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-09-14 15:20:16 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+
+import { fetchCountries } from './js/fetchlist';
+
+const DEBOUNCE_DELAY = 300;
+const inputValue = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+inputValue.addEventListener('input', debounce(valueInInput, DEBOUNCE_DELAY));
+
+function valueInInput(e) {
+  let countryName = e.target.value.trim();
+  const newFetch = fetchCountries(countryName);
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+
+  if (countryName)
+    newFetch.then(data => {
+      if (data.length >= 2 && data.length <= 10) renderCountryList(data);
+      if (data.length === 1) renderCountryInfo(data);
+    });
+}
+
+function renderCountryList(data) {
+  const newCountryList = data.reduce((acc, element) => {
+    acc += `<li><img src="${element.flags.svg}" alt="" width="30"><span> ${element.name}</span></li>`;
+    return acc;
+  }, '');
+  countryList.insertAdjacentHTML('beforeend', newCountryList);
+}
+
+function renderCountryInfo(data) {
+  countryList.innerHTML = '';
+  const newCountryInfo = data
+    .map(
+      element =>
+        `<img src="${element.flags.svg}" alt="flag" width="30"><span> <b>${
+          element.name
+        }</b></span>
+    <p><b>Capital:</b> ${element.capital}</p><p><b>Population:</b> ${
+          element.population
+        }</p><p><b>Lenguages:</b> ${element.languages.map(
+          element => element.name
+        )}</p>`
+    )
+    .join();
+  countryInfo.innerHTML = newCountryInfo;
+}
---
+++ src/js/fetchList.js
@@ -0,0 +1,22 @@
+
+import Notiflix from 'notiflix';
+export function fetchCountries(name) {
+  if (!name) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    return;
+  }
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.length > 10)
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      return data;
+    })
+    .catch(err => Notiflix.Notify.failure(err));
+}
